52bdd7e1442860eb543197b3cd20c60816f0bc78
a7medtech <a7medtech_nology@yahoo.com>
2018-05-10 15:47:29 -0700

    User already has an appointment

--- index.js
+++ index.js
@@ -200,7 +200,7 @@ app.post("/returnAvailableSlots", async (req, res) => {
     //Is the client already has a date?
     //Get timeframes referrence
     var timeFramesRef = await database.getDocumentFromCollection(bank, branch).collection('TimeFrames').get();
-    timeFramesRef.forEach(doc => {
+    timeFramesRef.forEach(async doc => {
       timeSlotReg = doc.ref.collection('TimeSlots');
       var UserApp = await timeSlotReg.get();
       try {
